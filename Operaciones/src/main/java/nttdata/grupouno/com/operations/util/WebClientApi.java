e24f5aa538b83564ccca616962b5bb588e40be55
jcastillo20 <juancrcastillo20@gmail.com>
2022-09-28 16:02:58 -0500

    Funcionalidad Operaciones

--- Operaciones/src/main/java/nttdata/grupouno/com/operations/util/WebClientApi.java
+++ Operaciones/src/main/java/nttdata/grupouno/com/operations/util/WebClientApi.java
@@ -1,9 +1,11 @@
 package nttdata.grupouno.com.operations.util;
 
 import nttdata.grupouno.com.operations.models.MasterAccountModel;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
-
+@Repository
 public class WebClientApi {
     private WebClient webClient = WebClient.create("http://localhost:8001");
     
