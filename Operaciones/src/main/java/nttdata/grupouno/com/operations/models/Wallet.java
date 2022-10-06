e01dcd6a41edd41bb355baeb81af2795f2a2671f
Diego Maguino <diegomaguino2@gmail.com>
2022-10-06 17:16:04 -0500

    Monedero

---
+++ Operaciones/src/main/java/nttdata/grupouno/com/operations/models/Wallet.java
@@ -0,0 +1,22 @@
+package nttdata.grupouno.com.operations.models;
+
+import org.springframework.data.annotation.Id;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
+public class Wallet {
+    @Id
+    private Integer id;
+    @NotNull
+    private String docNumber;
+    @Positive
+    @NotNull
+    private Integer phoneNumber;
+    @NotNull
+    private String imei;
+    @NotNull
+    @Email(regexp = ".+[@].+[\\.].+")
+    private String mail;
+}
